effa7ae286122b7e00b4928862e9d97d604fd83c
Ольга <lakki717@gmail.com>
2024-10-28 00:07:21 +0300

    Добавлена метка с названием базовой валюты

--- Обмен валюты 2.py
+++ Обмен валюты 2.py
@@ -5,10 +5,17 @@ from tkinter import ttk
 from bottle import response
 
 
-def update_c_label(event):
+def update_b_label(event):
+    code = b_combobox.get()
+    name = cur[code]
+    b_label.config(text=name)
+
+
+def update_t_label(event):
     code = t_combobox.get()
     name = cur[code]
-    c_label.config(text=name)
+    t_label.config(text=name)
+
 
 def exchange():
     t_code = t_combobox.get()
@@ -27,10 +34,9 @@ def exchange():
 
                 mb.showinfo("Курсы обмена",f"Курс: {exchange_rate:.2f} {t_name} за 1 {b_name}")
             else:
-                mb.showerror("Ошибка",f"Валюта {code} не найдена")
+                mb.showerror("Ошибка",f"Валюта {t_code} не найдена")
         except Exception as e:
             mb.showerror("Ошибка",f"Произошла ошибка: {e}.")
-
     else:
         mb.showerror("Внимание!","Введите код валюты")
 cur = {
@@ -47,23 +53,24 @@ cur = {
     'USD': 'Американский доллар'
 }
 
+
 window = Tk()
 window.title("Курсы обмена валют")
-window.geometry("360x250")
+window.geometry("360x300")
 
 Label(text="Базовая валюта").pack(padx=10, pady=10)
 b_combobox = ttk.Combobox(values=list(cur.keys()))
 b_combobox.pack(padx=10,pady=10)
-# b_combobox.bind("<<ComboboxSelected>>", update_c_label)
+b_combobox.bind("<<ComboboxSelected>>", update_b_label)
+b_label=ttk.Label()
+b_label.pack(padx=10, pady=10)
 
 Label(text="Целевая валюта").pack(padx=10, pady=10)
-
 t_combobox = ttk.Combobox(values=list(cur.keys()))
 t_combobox.pack(padx=10,pady=10)
-t_combobox.bind("<<ComboboxSelected>>", update_c_label)
-
-c_label = ttk.Label()
-c_label.pack(padx=10, pady=10)
+t_combobox.bind("<<ComboboxSelected>>", update_t_label)
+t_label = ttk.Label()
+t_label.pack(padx=10, pady=10)
 
 
 Button(text="Получить курс обмена", command=exchange).pack(padx=10, pady=10)
